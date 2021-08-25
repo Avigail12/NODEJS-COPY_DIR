decdde9037cde1c03e46315d9be4d58a04c6a313
Avigail12 <Avigailcohen1111@gmail.com>
2021-08-25 08:41:52 +0300

    add validation

--- routes/copy.js
+++ routes/copy.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const copyFolder = require('../copyFiles');
 const router = express.Router();
+const fs = require('fs')
 
 
 router.get('/',(req,res) => {
@@ -12,17 +13,24 @@ router.post('/',(req,res) => {
     const { src_dir } = req.body
     const { des_dir } = req.body
 
-    if(!src_dir)
-    {
+    if(!src_dir){
         return res.status(400).json({ success: false, msg: 'please provide source directory' })
     }
 
-    if(!des_dir)
-    {
+    if(!des_dir){
         return res.status(400).json({ success: false, msg: 'please provide desteniton directory' })
     }
+
+    if (!fs.existsSync(src_dir)) {
+        return res.status(400).json({ success: false, msg: 'The source not exists' })
+    }
+
+    if (!fs.lstatSync(src_dir).isDirectory()){
+        return res.status(400).json({ success: false, msg: 'The source is not a library' })
+    }
+    
     copyFolder(src_dir,des_dir);
-    res.status(201).json({ success: true, src_dir: src_dir })
+    res.status(200).json({ success: true, src_dir: src_dir })
 })
 
 module.exports = router
